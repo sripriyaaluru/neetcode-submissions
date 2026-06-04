ecc17bbf29dbc60e13f0368e4f0075056497fd7a
sripriyaaluru <sripriya.aluru@gmail.com>
2026-06-04 19:43:24 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        #greedy
+        goal=len(nums)-1
+        for i in range(len(nums)-2,-1,-1):
+            if i+nums[i]>=goal:
+                goal=i
+            
+        return goal==0
+
+
+
+
+
+        
