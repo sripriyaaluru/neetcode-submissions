e1817690cb840302f932906f7d4bc69cb56b4a99
Sripriya Aluru <sripriya.aluru@gmail.com>
2026-06-19 15:25:56 +0530

    Add: meeting-schedule-ii - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-0.py
@@ -0,0 +1,32 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        start=sorted([i.start for i in intervals])
+        end=sorted([i.end for i in intervals])
+        res=count=0
+        s=e=0
+        while s<len(intervals): #start is what add to count end just decreases it so to get max rooms needed at a time we need only s
+            if start[s]<end[e]:
+                s+=1
+                count+=1
+                
+            elif start[s]>=end[e]:
+                 e+=1
+                 count-=1
+               
+            res=max(res,count)
+        return res 
+            
+
+
+
+      
+
+        
